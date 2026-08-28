7a308a446064d59a62e4fa6247e38d3a9be8038e
Aman Kumar jha <amanjha2470@gmail.com>
2026-08-28 15:21:33 +0530

    Update API_URL to include '/api' endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 // const API_URL = "http://localhost:5000/api";  
-const API_URL = "https://cine-graph-1ocb.onrender.com";  
+const API_URL = "https://cine-graph-1ocb.onrender.com/api";  
 
 function App() {
   const [actors, setActors] = useState([]);
